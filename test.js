6b1f76fbe31cfb8aedaa9572f19c1b99c5f6a9da
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2011-09-01 17:32:43 -0700

    initialize with IP and initial sandboxed path

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var ftpd = require('./ftpd.js');
 
-var server = ftpd.createServer("localhost").listen(7001, "127.0.0.1");
+var server = ftpd.createServer("127.0.0.1", "/home/alan/temporary").listen(7001);
