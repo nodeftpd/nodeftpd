0e8083aa53a24e221d33fce039560c994dc62fcd
Alex Drummond <a.d.drummond@gmail.com>
2012-05-12 11:09:23 +0100

    Switch to using 'readFile' instead of 'read' for downloads (not necessarily a good idea for a general purpose FTP server, but  when this is being used with sqlfs in nodeibexfarm, it makes more sense)

--- ftpd.js
+++ ftpd.js
@@ -713,34 +713,9 @@ function FtpServer(host, options) {
                         conn.totsize = 0;
                         conn.filename = filename;
                     }
-                    conn.fs.open( conn.filename, "r", function (err, fd) {
-                        logIf(0, "DATA file " + conn.filename + " opened", conn);
-                        socket.write("150 Opening " + conn.mode.toUpperCase() + " mode data connection\r\n");
-                        function readChunk() {
-                            if (! self.buffer) self.buffer = new Buffer(4096);
-                            conn.fs.read(fd, self.buffer, 0, 4096, null/*pos*/, function(err, bytesRead, buffer) {
-                                if(err) {
-                                    traceIf(0, "Error reading chunk", conn);
-                                    conn.emit("error", err);
-                                    return;
-                                }
-                                if (bytesRead > 0) {
-                                    conn.totsize += bytesRead;
-                                    if(pasvconn.readyState == "open") pasvconn.write(self.buffer.slice(0, bytesRead), conn.mode);
-                                    readChunk();
-                                }
-                                else {
-                                    logIf(0, "DATA file " + conn.filename + " closed", conn);
-                                    pasvconn.end();
-                                    socket.write("226 Closing data connection, sent " + conn.totsize + " bytes\r\n");
-                                    conn.fs.close(fd, function (err) {
-                                        if (err) conn.emit("error", err);
-                                        conn.totsize = 0;
-                                    });
-                                }
-                            });
-                        }
-                        if(err) {
+
+                    conn.fs.readFile(conn.filename, function (err, contents) {
+                        if (err) {
                             if (err.code == 'ENOENT') {
                                 socket.write("550 Not Found\r\n");
                             }
@@ -750,7 +725,12 @@ function FtpServer(host, options) {
                             }
                         }
                         else {
-                            readChunk();
+                            // TODO: This conditional was in the original code. Seems like there should also be
+                            // an 'else'. What do do here?
+                            socket.write("150 Opening " + conn.mode.toUpperCase() + " mode data connection\r\n");
+                            if (pasvconn.readyState == 'open') pasvconn.write(contents, conn.mode)
+                            pasvconn.end();
+                            socket.write("226 Closing data connection, sent " + conn.totsize + " bytes\r\n");
                         }
                     });
                 });
