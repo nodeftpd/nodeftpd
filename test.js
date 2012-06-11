b60059a7eb3f0d3ad609187704816db3dc310af9
Thomas Schaaf <schaaf@komola.de>
2012-06-11 15:48:13 +0200

    New file:received hook

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var ftpd = require('./ftpd.js');
 var fs = require('fs');
 var path = require('path');
 
+
 var server = new ftpd.FtpServer("127.0.0.1", {
 //    getInitialCwd: function () { return "/"; }
     getRoot: function () { return process.cwd(); },
@@ -13,19 +14,23 @@ var server = new ftpd.FtpServer("127.0.0.1", {
 // but should recommend they don't do socket operations on it
 // so should probably encapsulate and hide it
 server.on("client:connected", function(conn) {
-    var username = null;
-    console.log("client connected: " + conn.remoteAddress);
-    conn.on("command:user", function(user, success, failure) {
-        if (user) {
-            username = user;
-            success();
-        } else failure();
-    });
+  var username = null;
+  console.log("client connected: " + conn.remoteAddress);
+  conn.on("command:user", function(user, success, failure) {
+      if (user) {
+          username = user;
+          success();
+      } else failure();
+  });
 
-    conn.on("command:pass", function(pass, success, failure) {
-        if (pass) success(username);
-        else failure();
-    });
+  conn.on("command:pass", function(pass, success, failure) {
+      if (pass) success(username);
+      else failure();
+  });
+
+  conn.on("file:received", function(file) {
+    console.log(file + "this file was received");
+  });
 });
 server.debugging = 4;
-server.listen(7001);
+server.listen(7002);
