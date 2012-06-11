6537c357105f9cc05a47f1bd3d0607ac1a750d2f
Thomas Schaaf <schaaf@komola.de>
2012-06-11 15:49:56 +0200

    Just some testing for upload

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var ftpd = require('./ftpd.js');
 var fs = require('fs');
 var path = require('path');
+var multiparter = require("multiparter");
+var mime = require("mime");
+var url = require("url");
+var path = require("path");
+var http = require("http");
 
 
 var server = new ftpd.FtpServer("127.0.0.1", {
@@ -29,7 +34,45 @@ server.on("client:connected", function(conn) {
   });
 
   conn.on("file:received", function(file) {
-    console.log(file + "this file was received");
+    var post = "http://localhost/photos";
+    var settings = url.parse(post);
+    console.log(settings);
+    var request = new multiparter.request(http, {
+      host: settings.hostname,
+      port: settings.post, 
+      path: settings.pathname,
+      method: "POST"
+    });
+
+
+    request.addStream(
+      'file', 
+      path.basename(file),
+      mime.lookup(file),
+      fs.statSync(file).size,
+      fs.createReadStream(file));
+
+    request.send(function(error, response) {
+      if (error) {
+        console.log(error);
+      }
+
+      var data = "";
+
+      response.setEncoding("utf8");
+
+      response.on("data", function(chunk) {
+        data += chunk;
+      });
+
+      response.on("end", function() {
+        console.log("Data: " + data);
+      });
+
+      response.on("error", function(error) {
+        console.log(error);
+      });
+    });
   });
 });
 server.debugging = 4;
