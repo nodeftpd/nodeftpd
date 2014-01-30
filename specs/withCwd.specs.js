102cd54de5c6af4c94468e1933369acc26f1879f
Alex Drummond <a.d.drummond@gmail.com>
2014-01-30 13:27:23 -0500

    Extend tests for withCwd (handle Windows value for path.sep)

--- specs/withCwd.specs.js
+++ specs/withCwd.specs.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var path = require('path');
+// Hack to make it possible to test Windows behavior.
+path.sep = '\\';
 var withCwd = require('../lib/ftpd').withCwd;
 
 describe('withCwd', function () {
@@ -14,4 +17,19 @@ describe('withCwd', function () {
     assert.equal(withCwd('/foo/bar', '/../amp'), '/foo/bar/amp');
     assert.equal(withCwd('/foo/bar/', '../amp'), '/foo/bar/amp');
     assert.equal(withCwd('/foo/bar/', '/../amp'), '/foo/bar/amp');
+    assert.equal(withCwd('/foo/bar/', '/../amp/../../foobar'), '/foo/bar/amp/foobar');
+
+    assert.equal(withCwd('', ''), '/');
+    assert.equal(withCwd('\\', ''), '/');
+    assert.equal(withCwd('', '\\'), '/');
+    assert.equal(withCwd('\\', '\\'), '/');
+    assert.equal(withCwd('\\foo', 'bar'), '\\foo/bar');
+    assert.equal(withCwd('\\foo', '\\bar'), '\\foo/bar');
+    assert.equal(withCwd('\\foo', '\\..\\bar'), '\\foo/bar');
+    assert.equal(withCwd('\\foo', '..\\bar'), '\\foo/bar');
+    assert.equal(withCwd('\\foo\\bar', '..\\amp'), '\\foo\\bar/amp');
+    assert.equal(withCwd('\\foo\\bar', '\\..\\amp'), '\\foo\\bar/amp');
+    assert.equal(withCwd('\\foo\\bar\\', '..\\amp'), '\\foo\\bar/amp');
+    assert.equal(withCwd('\\foo\\bar\\', '\\..\\amp'), '\\foo\\bar/amp');
+    assert.equal(withCwd('\\foo\\bar\\', '\\..\\amp\\..\\..\\foobar'), '\\foo\\bar/amp/foobar');
 });
