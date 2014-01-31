2faa6452b5027e461b57483ad452d1a38c1f1772
asylumfunk <asylumfunk@users.noreply.github.com>
2014-01-30 23:56:10 -0800

    refactor test.js
    remove obsolete dependencies
    remove commented-out code
    hoist variable declarations
    move option literals to variable object
    correct spelling mistake
    compare with strict inequality
    use braces consistently
    use quotation marks consistently
    shorten long lines
    add newline to end-of-file
    
    fix asylumfunk#11

--- test.js
+++ test.js
@@ -1,77 +1,83 @@
-var ftpd = require('./');
-var fs = require('fs');
-var path = require('path');
-var multiparter = require("multiparter");
-var mime = require("mime");
-var url = require("url");
-var path = require("path");
-var http = require("http");
+var ftpd = require('./'),
+  fs = require('fs'),
+  path = require('path'),
+  keyFile,
+  certFile,
+  server,
+  options = {
+    host: process.env.IP || '127.0.0.1',
+    port: process.env.PORT || 7002,
+    tls: null
+  };
 
-var keyFile, certFile;
 if (process.env.KEY_FILE && process.env.CERT_FILE) {
-  console.log("Running as FPTS server");
-  if (process.env.KEY_FILE.charAt(0) != '/')
+  console.log('Running as FTPS server');
+  if (process.env.KEY_FILE.charAt(0) !== '/') {
     keyFile = path.join(__dirname, process.env.KEY_FILE);
-  if (process.env.CERT_FILE.charAt(0) != '/')
+  }
+  if (process.env.CERT_FILE.charAt(0) !== '/') {
     certFile = path.join(__dirname, process.env.CERT_FILE);
+  }
+  options.tls = {
+    key: fs.readFileSync(keyFile),
+    cert: fs.readFileSync(certFile),
+    ca: !process.env.CA_FILES ? null : process.env.CA_FILES
+      .split(':')
+      .map(function (f) {
+        return fs.readFileSync(f);
+      })
+  };
 }
 else {
-  console.log(
-      "\n" +
-          "*** To run as FTPS server, set 'KEY_FILE', 'CERT_FILE' and (optionally) ***\n" +
-          "*** 'CA_FILES' env vars. Set 'PORT' to change port that the server      ***\n" +
-          "*** listens on.                                                         ***\n"
-  );
+  console.log();
+  console.log('*** To run as FTPS server,                 ***');
+  console.log('***  set "KEY_FILE", "CERT_FILE"           ***');
+  console.log('***  and (optionally) "CA_FILES" env vars. ***');
+  console.log();
 }
-var listenPort = process.env.PORT || 7002;
 
-var tlsOptions = (process.env.KEY_FILE && process.env.CERT_FILE ? {
-  key: fs.readFileSync(keyFile),
-  cert: fs.readFileSync(certFile),
-  ca: !process.env.CA_FILES ? null : process.env.CA_FILES.split(':').map(function(f) {
-    return fs.readFileSync(f);
-  })
-} : null);
-
-var server = new ftpd.FtpServer(process.env.IP || "127.0.0.1", {
-  //    getInitialCwd: function (username, callback) { callback(null, "/"); },
-  //    getRoot: function (username, callback) { callback(null, process.cwd()); },
-  getInitialCwd: function() {
-    return "/";
+server = new ftpd.FtpServer(options.host, {
+  getInitialCwd: function () {
+    return '/';
   },
-  getRoot: function() {
+  getRoot: function () {
     return process.cwd();
   },
   pasvPortRangeStart: 1025,
   pasvPortRangeEnd: 1050,
-  tlsOptions: tlsOptions,
-  //    tlsOnly: true,
+  tlsOptions: options.tls,
   allowUnauthorizedTls: true,
   useWriteFile: false,
   useReadFile: false,
   uploadMaxSlurpSize: 7000 // N/A unless 'useWriteFile' is true.
 });
 
-server.on('error', function(err) {
-  console.log("FTP Server error:", err);
+server.on('error', function (error) {
+  console.log('FTP Server error:', error);
 });
 
-server.on("client:connected", function(conn) {
+server.on('client:connected', function (connection) {
   var username = null;
-  console.log("client connected: " + conn.remoteAddress);
-  conn.on("command:user", function(user, success, failure) {
+  console.log('client connected: ' + connection.remoteAddress);
+  connection.on('command:user', function (user, success, failure) {
     if (user) {
       username = user;
       success();
-    } else failure();
+    } else {
+      failure();
+    }
   });
 
-  conn.on("command:pass", function(pass, success, failure) {
-    if (pass) success(username);
-    else failure();
+  connection.on('command:pass', function (pass, success, failure) {
+    if (pass) {
+      success(username);
+    } else {
+      failure();
+    }
   });
 });
-server.debugging = 4;
-server.listen(listenPort);
 
-console.log("Listening on port " + listenPort);
+server.debugging = 4;
+server.listen(options.port);
+console.log('Listening on port ' + options.port);
+
