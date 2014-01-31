d37470c8fefeebbbe17ec1a3d1f29ac733ea6c23
Alex Drummond <a.d.drummond@gmail.com>
2014-01-30 19:43:33 -0500

    Revert "Extend tests for withCwd (handle Windows value for path.sep)"
    This reverts commit 102cd54de5c6af4c94468e1933369acc26f1879f.
    
    https://github.com/sstur/nodeftpd/commit/57a9e5f9e0c9fd3e5fbdd7f847d5f0116dbd5a4a#commitcomment-5232986

--- specs/withCwd.specs.js
+++ specs/withCwd.specs.js
@@ -1,7 +1,4 @@
 var assert = require('assert');
-var path = require('path');
-// Hack to make it possible to test Windows behavior.
-path.sep = '\\';
 var withCwd = require('../lib/ftpd').withCwd;
 
 describe('withCwd', function () {
@@ -17,19 +14,4 @@ describe('withCwd', function () {
     assert.equal(withCwd('/foo/bar', '/../amp'), '/foo/bar/amp');
     assert.equal(withCwd('/foo/bar/', '../amp'), '/foo/bar/amp');
     assert.equal(withCwd('/foo/bar/', '/../amp'), '/foo/bar/amp');
-    assert.equal(withCwd('/foo/bar/', '/../amp/../../foobar'), '/foo/bar/amp/foobar');
-
-    assert.equal(withCwd('', ''), '/');
-    assert.equal(withCwd('\\', ''), '/');
-    assert.equal(withCwd('', '\\'), '/');
-    assert.equal(withCwd('\\', '\\'), '/');
-    assert.equal(withCwd('\\foo', 'bar'), '\\foo/bar');
-    assert.equal(withCwd('\\foo', '\\bar'), '\\foo/bar');
-    assert.equal(withCwd('\\foo', '\\..\\bar'), '\\foo/bar');
-    assert.equal(withCwd('\\foo', '..\\bar'), '\\foo/bar');
-    assert.equal(withCwd('\\foo\\bar', '..\\amp'), '\\foo\\bar/amp');
-    assert.equal(withCwd('\\foo\\bar', '\\..\\amp'), '\\foo\\bar/amp');
-    assert.equal(withCwd('\\foo\\bar\\', '..\\amp'), '\\foo\\bar/amp');
-    assert.equal(withCwd('\\foo\\bar\\', '\\..\\amp'), '\\foo\\bar/amp');
-    assert.equal(withCwd('\\foo\\bar\\', '\\..\\amp\\..\\..\\foobar'), '\\foo\\bar/amp/foobar');
 });
