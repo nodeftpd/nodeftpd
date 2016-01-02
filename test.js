751835da27b33966ae1f61e9710d9d3e9e4c14f8
Simon Sturmer <sstur@me.com>
2016-01-02 08:24:00 +0700

    fix lint warnings in test.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-var ftpd = require('./'),
-  fs = require('fs'),
-  path = require('path'),
-  keyFile,
-  certFile,
-  server,
-  options = {
-    host: process.env.IP || '127.0.0.1',
-    port: process.env.PORT || 7002,
-    tls: null
-  };
+var ftpd = require('./');
+var fs = require('fs');
+var path = require('path');
+var keyFile;
+var certFile;
+var server;
+var options = {
+  host: process.env.IP || '127.0.0.1',
+  port: process.env.PORT || 7002,
+  tls: null
+};
 
 if (process.env.KEY_FILE && process.env.CERT_FILE) {
   console.log('Running as FTPS server');
@@ -23,12 +23,11 @@ if (process.env.KEY_FILE && process.env.CERT_FILE) {
     cert: fs.readFileSync(certFile),
     ca: !process.env.CA_FILES ? null : process.env.CA_FILES
       .split(':')
-      .map(function (f) {
+      .map(function(f) {
         return fs.readFileSync(f);
       })
   };
-}
-else {
+} else {
   console.log();
   console.log('*** To run as FTPS server,                 ***');
   console.log('***  set "KEY_FILE", "CERT_FILE"           ***');
@@ -37,10 +36,10 @@ else {
 }
 
 server = new ftpd.FtpServer(options.host, {
-  getInitialCwd: function () {
+  getInitialCwd: function() {
     return '/';
   },
-  getRoot: function () {
+  getRoot: function() {
     return process.cwd();
   },
   pasvPortRangeStart: 1025,
@@ -52,14 +51,14 @@ server = new ftpd.FtpServer(options.host, {
   uploadMaxSlurpSize: 7000 // N/A unless 'useWriteFile' is true.
 });
 
-server.on('error', function (error) {
+server.on('error', function(error) {
   console.log('FTP Server error:', error);
 });
 
-server.on('client:connected', function (connection) {
+server.on('client:connected', function(connection) {
   var username = null;
   console.log('client connected: ' + connection.remoteAddress);
-  connection.on('command:user', function (user, success, failure) {
+  connection.on('command:user', function(user, success, failure) {
     if (user) {
       username = user;
       success();
@@ -68,7 +67,7 @@ server.on('client:connected', function (connection) {
     }
   });
 
-  connection.on('command:pass', function (pass, success, failure) {
+  connection.on('command:pass', function(pass, success, failure) {
     if (pass) {
       success(username);
     } else {
@@ -80,4 +79,3 @@ server.on('client:connected', function (connection) {
 server.debugging = 4;
 server.listen(options.port);
 console.log('Listening on port ' + options.port);
-
