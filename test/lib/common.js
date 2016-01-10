e031b3c890261580b3b707e7138df54cce79c964
Simon Sturmer <sstur@me.com>
2016-01-10 17:41:08 +0800

    fixes to test/lib/common.js

--- test/lib/common.js
+++ test/lib/common.js
@@ -1,9 +1,30 @@
+'use strict';
+
 var path = require('path');
 var fs = require('fs');
-var Server = require('../../').FtpServer;
+var ftpd = require('../../');
 var Client = require('jsftp');
 var should = require('should');
+
+var Server = ftpd.FtpServer;
+var LogLevels = ftpd.LOG_LEVELS;
+var LogLevelNames = Object.keys(LogLevels).reduce(function(map, name) {
+  var value = LogLevels[name];
+  map[value] = name;
+  return map;
+}, {});
+
 var fixturesPath = path.join(__dirname, '../../fixture');
+
+function toString(value) {
+  var isPrimitive = Object(value) !== value;
+  if (isPrimitive) {
+    return JSON.stringify(value);
+  } else {
+    return ('toString' in value) ? value.toString() : Object.prototype.toString(value);
+  }
+}
+
 var options = {
   host: process.env.IP || '127.0.0.1',
   port: process.env.port || 7002,
@@ -22,21 +43,20 @@ var options = {
 
 module.exports = {
   should: should,
+
   fixturesPath: function() {
     return fixturesPath;
   },
+
   server: function(customOptions) {
-    'use strict';
-    var srv;
-    var origLogIf;
     customOptions = customOptions || {};
     Object.keys(options).forEach(function(key) {
       if (!customOptions.hasOwnProperty(key)) {
         customOptions[key] = options[key];
       }
     });
-    srv = new Server(customOptions.host, customOptions);
-    srv.on('client:connected', function(connection) {
+    var server = new Server(customOptions.host, customOptions);
+    server.on('client:connected', function(connection) {
       var username;
       connection.on('command:user', function(user, success, failure) {
         if (user === customOptions.user) {
@@ -45,7 +65,8 @@ module.exports = {
         } else {
           failure();
         }
-      }).on('command:pass', function(pass, success, failure) {
+      });
+      connection.on('command:pass', function(pass, success, failure) {
         if (pass === customOptions.pass) {
           success(username);
         } else {
@@ -53,44 +74,39 @@ module.exports = {
         }
       });
     });
-    origLogIf = srv._logIf;
-    srv.suppressExpecteErrMsgs = [];
-    srv._logIf = function logIfNotExpected(verbosity, msg, conn, isErr) {
-      // TODO: remove this when we implement better noise control for logging.
-      if (msg === 'Client connection closed') {
-        return;
-      }
-      var xpct = srv.suppressExpecteErrMsgs;
-      msg = String(msg);
-      msg = msg.replace(new RegExp(fixturesPath.replace(/(\W)/g, '\\$1'),
-        'g'), 'fixture:/');
-      if ((xpct.length > 0) && (verbosity < 2)) {
-        xpct = xpct.shift();
-        if (msg === xpct) {
+    var origLogIf = server._logIf;
+    server.suppressExpecteErrMsgs = [];
+    server._logIf = function logIfNotExpected(verbosity, message, conn) {
+      var expecteErrMsgs = server.suppressExpecteErrMsgs;
+      message = String(message).split(fixturesPath).join('fixture:/');
+      if ((expecteErrMsgs.length > 0) && (verbosity < LogLevels.LOG_INFO)) {
+        var expected = expecteErrMsgs.shift();
+        if (message === expected) {
           return;
         }
-        if ((xpct instanceof RegExp) && xpct.test(msg)) {
+        if ((expected instanceof RegExp) && expected.test(message)) {
           return;
         }
-        if ((typeof xpct) === 'function') {
-          msg = xpct(msg);
-          if (msg === '') {
+        if ((typeof expected) === 'function') {
+          message = expected(message);
+          if (message === '') {
             return;
           }
         } else {
-          console.error('\nExpected log msg\t' +
-            ((typeof xpct) === 'string' ? JSON.stringify(xpct) : String(xpct)));
-          console.error('did not match [' + verbosity + ']\t' +
-            JSON.stringify(msg));
+          console.error(
+            '\nExpected log message:\n' + toString(expected) + '\n' +
+            'did not match [' + LogLevelNames[verbosity] + ']:\n' +
+            JSON.stringify(message)
+          );
         }
       }
-      return origLogIf.call(this, verbosity, msg, conn, isErr);
+      return origLogIf.call(this, verbosity, message, conn);
     };
-    srv.listen(customOptions.port);
-    return srv;
+    server.listen(customOptions.port);
+    return server;
   },
+
   client: function(done, customOptions) {
-    'use strict';
     customOptions = customOptions || {};
     Object.keys(options).forEach(function(key) {
       if (!customOptions.hasOwnProperty(key)) {
@@ -101,13 +117,16 @@ module.exports = {
       host: customOptions.host,
       port: customOptions.port,
     });
-    client.auth(customOptions.user, customOptions.pass,
+    client.auth(
+      customOptions.user,
+      customOptions.pass,
       function(error, response) {
         should.not.exist(error);
         should.exist(response);
         response.should.have.property('code', 230);
         done();
-      });
+      }
+    );
     return client;
   },
 };
