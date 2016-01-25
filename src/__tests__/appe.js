97ba57f6f03de4ebb2ece7e75e7a3e437a8c267d
Simon Sturmer <sstur@me.com>
2016-01-25 09:29:16 +0700

    Better APPE tests

--- src/__tests__/appe.js
+++ src/__tests__/appe.js
@@ -6,42 +6,78 @@ var fs = require('fs');
 describe('APPE command', () => {
   var client = new FtpClient();
   var server;
+  var fileToUpload = __filename;
+  var savedFileName = path.basename(fileToUpload) + '.test.txt';
+  var savedFilePublicPath = '/uploads/' + savedFileName;
+  var savedFilePrivatePath = path.join(
+    common.fixturesPath(),
+    common.defaultOptions().user,
+    'uploads',
+    savedFileName
+  );
 
-  //run tests both ways
-  [true, false].forEach((useWriteFile) => {
+  //run tests with various option combinations
+  let optionCombinations = [
+    {useWriteFile: false},
+    {useWriteFile: true},
+    // This will cause writeFile to fall back to using a stream.
+    {useWriteFile: true, uploadMaxSlurpSize: 1},
+  ];
+  optionCombinations.forEach((options) => {
 
-    describe('with useWriteFile = ' + useWriteFile, () => {
+    describe('with ' + JSON.stringify(options), () => {
 
       beforeEach((done) => {
-        server = common.server({useWriteFile: useWriteFile});
+        server = common.server(options);
         client.once('ready', () => {
           done();
         });
-        client.connect({
-          host: common.defaultOptions().host,
-          port: common.defaultOptions().port,
-          user: common.defaultOptions().user,
-          password: common.defaultOptions().pass,
-        });
+        let {host, port, user, pass} = common.defaultOptions();
+        client.connect({host, port, user, password: pass});
       });
 
       it('should append data to existing file', (done) => {
-        var basename = path.basename(__filename);
-        var fileSize = fs.statSync(__filename).size;
-        client.put(__filename, '/uploads/' + basename, (err) => {
-          common.should.not.exist(err);
-          client.append(__filename, '/uploads/' + basename, (err) => {
-            common.should.not.exist(err);
-            var filePath = path.join(
-              common.fixturesPath(),
-              common.defaultOptions().user,
-              'uploads',
-              basename
-            );
-            var newSize = fs.statSync(filePath).size;
-            newSize.should.be.eql(fileSize * 2);
-            done();
+        var removeFile = (callback) => {
+          fs.unlink(savedFilePrivatePath, (error) => {
+            common.should.not.exist(error);
+            callback();
           });
+        };
+
+        var doTest = () => {
+          fs.stat(fileToUpload, (error, stat) => {
+            common.should.not.exist(error);
+            var fileSize = stat.size;
+            client.put(fileToUpload, savedFilePublicPath, (error) => {
+              common.should.not.exist(error);
+              fs.stat(savedFilePrivatePath, (error, stat) => {
+                common.should.not.exist(error);
+                stat.size.should.eql(fileSize);
+                client.append(fileToUpload, savedFilePublicPath, (error) => {
+                  common.should.not.exist(error);
+                  fs.stat(savedFilePrivatePath, (error, stat) => {
+                    common.should.not.exist(error);
+                    var newSize = stat.size;
+                    newSize.should.eql(fileSize * 2);
+                    removeFile(done);
+                  });
+                });
+              });
+            });
+          });
+        };
+
+        fs.stat(savedFilePrivatePath, (error) => {
+          if (error) {
+            if (error.code !== 'ENOENT') {
+              throw error;
+            }
+            // The file doesn't exist. This is expected. Proceed with test.
+            doTest();
+          } else {
+            // The file exists. Delete it and then proceed with test.
+            removeFile(doTest);
+          }
         });
       });
 
