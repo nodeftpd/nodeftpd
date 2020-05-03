2f41504be5cc30129f736c0fc7ac6f98cffd895a
Stephan van Rooij <github@svrooij.nl>
2020-05-03 21:38:47 +0200

    chore: Test retry

--- jest.config.js
+++ jest.config.js
@@ -159,6 +159,7 @@ module.exports = {
 
   // This option allows use of a custom test runner
   // testRunner: "jasmine2",
+  testTimeout: 10000,
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   // testURL: "http://localhost",
